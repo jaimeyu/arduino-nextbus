eb8babac4bfa3a89532eb117cfe0b06b83e3a893
Jaime Yu <jaime@jaimeyu.com>
2014-03-31 22:12:41 -0400

    Initial test to make sure the libraries are accessible by the compiler. I used symbolic links so I can easily track and make changes to the libraries if I need to.

---
+++ nextbus/nextbus.ino
@@ -0,0 +1,16 @@
+#include <Adafruit_CC3000.h>
+#include <Adafruit_CC3000_Server.h>
+#include <ccspi.h>
+
+#include <EPD/EPD.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
